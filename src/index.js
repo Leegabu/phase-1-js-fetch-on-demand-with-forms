94b5aaab9a69358468d46454e56a5a7251a358fc
Lee <leeirungu11313@gmail.com>
2024-04-04 12:09:40 +0300

    Adding index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm = document.addEventListener("submit", (e) => {
+    e.preventDefault()
+   /* e.target.children[1].value;*/ // method of getting in put through event object
+   const input = document.querySelector("input#searchByID") // method two get value directly
+   fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then((data) => {
+        const title = document.querySelector("section#movieDetails h4")
+        const summary = document.querySelector("sector#movieDetails p")
+        title.innerHTML = data.title
+        summary.innerHTML = data.summary
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
